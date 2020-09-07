b34bba44c4bc95aa082acefd61b408dcfa71f610
Yun Chenxi <cleoxiii@163.com>
2020-09-07 21:49:21 +0800

    [yunchenxi] feat: should add new student into student list.

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentController.java
@@ -2,9 +2,7 @@ package com.thoughtworks.capability.gtb.entrancequiz.api;
 
 import com.thoughtworks.capability.gtb.entrancequiz.domain.Student;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +12,14 @@ import java.util.List;
 public class StudentController {
     public static List<Student> studentList;
 
+    public StudentController() {
+        initStudentList();
+    }
+
     private void initStudentList() {
         studentList = new ArrayList<>();
-        String[] students = new String[] {
-            "沈乐棋", "徐慧慧", "陈思聪", "王江林", "王登宇", "杨思雨", "江雨舟", "廖燊", "胡晓", "但杰", "盖迈达", "肖美琦",
+        String[] students = new String[]{
+                "沈乐棋", "徐慧慧", "陈思聪", "王江林", "王登宇", "杨思雨", "江雨舟", "廖燊", "胡晓", "但杰", "盖迈达", "肖美琦",
                 "黄云洁", "齐瑾浩", "刘亮亮", "肖逸凡", "王作文", "郭瑞凌", "李明豪", "党泽", "肖伊佐", "贠晨曦", "李康宁",
                 "马庆", "商婕", "余榕", "谌哲", "董翔锐", "陈泰宇", "赵允齐", "张柯", "廖文强", "刘轲", "廖浚斌", "凌凤仪"
         };
@@ -30,10 +32,14 @@ public class StudentController {
         }
     }
 
-
     @GetMapping("/student-list")
     public ResponseEntity<List<Student>> getStudentList() {
-        initStudentList();
         return ResponseEntity.ok(studentList);
+    }
+
+    @PostMapping("/student")
+    public ResponseEntity<Object> addStudent(@RequestBody String student) {
+        studentList.add(new Student(student));
+        return ResponseEntity.created(null).build();
     }
 }
--- src/test/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentControllerTest.java
@@ -1,26 +1,47 @@
 package com.thoughtworks.capability.gtb.entrancequiz.api;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class StudentControllerTest {
     @Autowired
     MockMvc mockMvc;
 
     @Test
+    @Order(1)
     void should_get_student_list() throws Exception {
         mockMvc.perform(get("/student-list"))
                 .andExpect(jsonPath("$", hasSize(35)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Order(2)
+    void should_add_new_student() throws Exception {
+        String jsonStr = "张三";
+
+        mockMvc.perform(post("/student").content(jsonStr).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+        mockMvc.perform(get("/student-list"))
+                .andExpect(jsonPath("$", hasSize(36)))
+                .andExpect(jsonPath("$[35].studentName", is("张三")))
                 .andExpect(status().isOk());
     }
 }
